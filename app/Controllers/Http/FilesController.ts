8fd8e3f450997dd3cd22cdef1898fb05d0731cc9
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-07-06 16:13:00 -0500

    Removes datadrive

--- app/Controllers/Http/FilesController.ts
+++ app/Controllers/Http/FilesController.ts
@@ -1,17 +1,35 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import DataDrive from '@ioc:DataDrive';
+import { StorageManager, LocalFileSystemStorage } from '@slynova/flydrive';
+
+const storageConfig = {
+  default: 'local',
+  disks: {
+    local: { driver: 'local', config: { root: `${process.cwd()}/data` } },
+
+    s3: {
+      driver: 's3',
+      config: {
+        key: 'AWS_S3_KEY',
+        secret: 'AWS_S3_SECRET',
+        region: 'AWS_S3_REGION',
+        bucket: 'AWS_S3_BUCKET',
+      },
+    },
+  },
+};
 
 export default class FilesController {
-  private dataPath = './data';
+  private storage = new StorageManager(storageConfig).disk<
+    LocalFileSystemStorage
+  >('local');
 
   public async upload({ request, params }: HttpContextContract) {
     if (!request.hasValidSignature()) return { error: 'url not valid' };
 
     const { filename } = params;
     if (!filename) return { error: 'filename is required' };
-
     request.multipart.onFile(filename, {}, async (file) => {
-      await DataDrive.drive(this.dataPath).put(filename, file);
+      await this.storage.put(filename, file);
     });
     await request.multipart.process();
 
@@ -24,8 +42,8 @@ export default class FilesController {
     const { filename } = params;
     if (!filename) return { error: 'filename is required' };
 
-    const file = await DataDrive.drive(this.dataPath).get(filename);
+    const file = await this.storage.get(filename);
     if (!file) return { error: `file ${filename} not founded` };
-    return { data: file };
+    return file;
   }
 }
